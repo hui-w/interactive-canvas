836fc2d2d8e443367344ced5c1f856d4e05964a4
hui-w <huiwang@qlike.com>
2017-04-24 15:24:42 +0800

    Use docs folder instead for gh-pages

--- tools/build.sh
+++ tools/build.sh
@@ -2,7 +2,7 @@
 
 YUI="./yuicompressor-2.4.8.jar"
 SRC="../src"
-OUT="../output"
+OUT="../release"
 
 echo "Deleting old content of output..."
 rm -rf $OUT
@@ -36,17 +36,5 @@ $OUT/temp.minified.js \
 echo "Deleting temporary files..."
 rm -f $OUT/temp.combined.js
 rm -f $OUT/temp.minified.js
-
-echo "Preparing files for web site..."
-mkdir $OUT/pages
-cp $OUT/interactive-canvas-0.1.1.min.js $OUT/pages/interactive-canvas-0.1.1.min.js
-cp ../pages/demo.js $OUT/pages/demo.js
-cp ../pages/index_release.html $OUT/pages/index.html
-cp ../pages/overview.png $OUT/pages/overview.png
-cp ../pages/palette.js $OUT/pages/palette.js
-cp ../pages/reference.html $OUT/pages/reference.html
-cp ../pages/responsive-app.js $OUT/pages/responsive-app.js
-cp ../pages/responsive_release.html $OUT/pages/responsive.html
-cp ../pages/web.css $OUT/pages/web.css
 
 echo "Done!"
