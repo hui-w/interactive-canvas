a36dc32e8907c198e4c14769649f7e84ea7cd254
hui-w <huiwang@qlike.com>
2017-04-24 15:30:30 +0800

    Update build script

--- tools/build.sh
+++ tools/build.sh
@@ -1,40 +1,45 @@
 #!/bin/sh
 
 YUI="./yuicompressor-2.4.8.jar"
-SRC="../src"
-OUT="../release"
+FILE_NAME="interactive-canvas-0.1.1.min.js"
+SRC_DIR="../src"
+OUT_DIR="../release"
+DOC_DIR="../docs"
 
 echo "Deleting old content of output..."
-rm -rf $OUT
-mkdir $OUT
+rm -rf $OUT_DIR
+mkdir $OUT_DIR
 
 echo "Combining JS files..."
 cat \
-$SRC/util/class.js \
-$SRC/util/polyfill.js \
-$SRC/util/event-handler.js \
-$SRC/util/list.js \
-$SRC/component.js \
-$SRC/panel.js \
-$SRC/label.js \
-$SRC/button.js \
-$SRC/canvas.js \
-> $OUT/temp.combined.js
+$SRC_DIR/util/class.js \
+$SRC_DIR/util/polyfill.js \
+$SRC_DIR/util/event-handler.js \
+$SRC_DIR/util/list.js \
+$SRC_DIR/component.js \
+$SRC_DIR/panel.js \
+$SRC_DIR/label.js \
+$SRC_DIR/button.js \
+$SRC_DIR/canvas.js \
+> $OUT_DIR/temp.combined.js
 
 echo "Compressing JS files..."
 java -jar "$YUI" \
 --type js \
--o $OUT/temp.minified.js \
-$OUT/temp.combined.js
+-o $OUT_DIR/temp.minified.js \
+$OUT_DIR/temp.combined.js
 
 echo "Adding the version header..."
 cat \
 ./version.js \
-$OUT/temp.minified.js \
-> $OUT/interactive-canvas-0.1.1.min.js
+$OUT_DIR/temp.minified.js \
+> $OUT_DIR/$FILE_NAME
 
 echo "Deleting temporary files..."
-rm -f $OUT/temp.combined.js
-rm -f $OUT/temp.minified.js
+rm -f $OUT_DIR/temp.combined.js
+rm -f $OUT_DIR/temp.minified.js
+
+echo "Preparing docs..."
+cp $OUT_DIR/$FILE_NAME $DOC_DIR/$FILE_NAME
 
 echo "Done!"
